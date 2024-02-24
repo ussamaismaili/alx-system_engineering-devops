b8411b086fb206aae565d4a5579d3d804577afb6
Oussama ismaili <ismailiossama@gmail.com>
2024-02-24 03:58:35 +0100

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,28 +1,10 @@
-# install_flask.pp
+#Using Puppet, install flask from pip3.
 
-# Include the stdlib module for package management
-include stdlib
-
-# Define a class for installing Python and pip3
-class { 'python':
-  version => '3',
-}
-
-package { 'python3-pip':
-  ensure   => 'present',
-  provider => 'pip3',
-  require  => Class['python'],
-}
-
-# Install Flask version 2.1.0 and Werkzeug version 2.0.2 using pip3
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+ensure   => '2.1.0',
+provider => 'pip3',
 }
-
 package { 'Werkzeug':
-  ensure   => '2.0.2',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+ensure   => '2.1.1',
+provider => 'pip3',
 }
